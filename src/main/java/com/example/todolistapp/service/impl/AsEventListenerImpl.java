92339b5d054463f6bab1e1a7dcf434fb25026f81
Bitarovský Roman <bitaror@softec.sk>
2023-12-05 19:03:36 +0100

    add kafka ano

--- src/main/java/com/example/todolistapp/service/impl/AsEventListenerImpl.java
+++ src/main/java/com/example/todolistapp/service/impl/AsEventListenerImpl.java
@@ -4,17 +4,17 @@ import com.example.todolistapp.event.AsEvent;
 import com.example.todolistapp.service.TaskService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationListener;
-import org.springframework.stereotype.Component;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
 
-@Component
 @Slf4j
-public class AsEventListenerImpl implements ApplicationListener<AsEvent> {
+@Service
+public class AsEventListenerImpl {
 
     @Autowired
     TaskService taskService = new TaskServiceImpl();
 
-    @Override
+    @KafkaListener(topics = "as-topic", groupId = "as-group")
     public void onApplicationEvent(AsEvent event) {
         log.info("Received custom event: " + event.getEventData());
         taskService.createNew(event.getEventData());
